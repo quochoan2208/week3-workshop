63ae9764dc8ef0b0aa9ce2617c14658553b060a7
quochoan2208 <quochoan.hoang@griffithuni.edu.au>
2023-07-31 11:27:26 +1000

    express has been added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var express = requires('express');
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+app.use(express.static(__dirname + '/www'));
+
+let server = http.listen(3000, function(){
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("My First Nodejs Server!");
+    console.log("Server listenting on: " + host + " port: " + port);
+});
+app.get('/test',function (req,res){
+    res.sendFile(__dirname + '/www/test.html');
+});
